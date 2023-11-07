f0c982d84baf6d069d71050da4f4d659bdef1d44
Titoine59 <titoine.farthouat@student.junia.com>
2023-11-07 17:51:49 +0100

    accueil

--- library-site/src/app/page.tsx
+++ library-site/src/app/page.tsx
@@ -1,8 +1,11 @@
 import { FC, ReactElement } from 'react';
 
 const Home: FC = (): ReactElement => (
-  <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    Home page test
+  <main className="flex min-h-screen flex-col items-start justify-start p-24">
+    <h1 className="text-4xl font-bold mt-2">Bienvenue dans notre Bibliothèque en Ligne</h1>
+    <p className="text-xl mt-4">Découvrez notre vaste collection de livres et explorez les œuvres des auteurs les plus renommés.</p>
+    <img src="/images/image-bibliothèque.jpg" alt="Description de l'image" className="max-w-full" />
+    {/* Reste du contenu de la page */}
   </main>
 );
 
