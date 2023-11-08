3501f519ee4c616e97e096dc77e27163b4bd433d
Rémi PROTIN <remi.protin@student.junia.com>
2023-11-08 13:34:19 +0100

    controleur update

--- library-api/src/controllers/authors/author.controller.ts
+++ library-api/src/controllers/authors/author.controller.ts
@@ -1,4 +1,57 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Delete } from '@nestjs/common';
+import { PlainAuthorPresenter } from 'library-api/src/controllers/authors/author.presenter';
+import { AuthorId } from 'library-api/src/entities';
+import { AuthorUseCases } from 'library-api/src/useCases';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateAuthorDto } from './author.dto';
 
+ApiTags('authors');
 @Controller('authors')
-export class AuthorController {}
+export class AuthorController {
+  constructor(private readonly authorUseCases: AuthorUseCases) {}
+
+  @Get('/')
+  @ApiOperation({
+    description: 'Get all authors',
+    operationId: 'getAllAuthors',
+  })
+  @ApiOkResponse({
+    isArray: true,
+    type: PlainAuthorPresenter,
+  })
+  public async getAll(): Promise<PlainAuthorPresenter[]> {
+    const authors = await this.authorUseCases.getAllPlain();
+
+    return authors.map(PlainAuthorPresenter.from);
+  }
+
+  @Get('/:id')
+  @ApiOperation({
+    description: 'Retrouver un auteur par son ID',
+    operationId: 'getAuthor',
+  })
+  @ApiOkResponse({
+    type: PlainAuthorPresenter,
+  })
+  public async getById(
+    @Param('id') id: AuthorId,
+  ): Promise<PlainAuthorPresenter> {
+    const author = await this.authorUseCases.getById(id);
+
+    return PlainAuthorPresenter.from(author);
+  }
+
+  @Post()
+  public async createauthor(
+    @Body() input: CreateAuthorDto,
+  ): Promise<PlainAuthorPresenter> {
+    const author = await this.authorUseCases.create(input);
+
+    return PlainAuthorPresenter.from(author);
+  }
+
+  @Delete('/:id')
+  public async deleteById(@Param('id') id: AuthorId): Promise<void> {
+    await this.authorUseCases.deleteById(id);
+  }
+}
--- library-api/src/controllers/genres/genre.controller.ts
+++ library-api/src/controllers/genres/genre.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { GenrePresenter } from 'library-api/src/controllers/genres/genre.presenter';
+import { GenreUseCases } from 'library-api/src/useCases/genres/genre.useCases';
 
+@ApiTags('genres')
 @Controller('genres')
-export class GenreController {}
+export class GenreController {
+  constructor(private readonly genreUseCases: GenreUseCases) {}
+
+  @Get('/')
+  @ApiOperation({
+    description: 'Get all genres',
+    operationId: 'getAllGenres',
+  })
+  @ApiOkResponse({
+    isArray: true,
+    type: GenrePresenter,
+  })
+  public async getAll(): Promise<GenrePresenter[]> {
+    const result = await this.genreUseCases.getAll();
+
+    return result.map(GenrePresenter.from);
+  }
+}
