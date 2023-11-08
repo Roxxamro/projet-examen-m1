3f9c965e6fcf3a2add205b4fc9c4d5b146b7d209
Rémi PROTIN <remi.protin@student.junia.com>
2023-11-08 11:45:42 +0100

    swagger import

--- library-api/src/main.ts
+++ library-api/src/main.ts
@@ -1,13 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 
-async function bootstrap(): Promise<void> {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  const port = process.env.API_PORT ?? 3000;
-  await app.listen(port);
-}
+  const config = new DocumentBuilder()
+  .setTitle('Library API')
+  .setDescription('API use')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`## App started on port ${port}`);
+}
 bootstrap();
