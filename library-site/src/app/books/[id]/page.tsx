6b13866dafe102cf158e73d68afce97ae5389277
Quentin AMBROZIEWICZ <quentin.ambroziewicz@student.junia.com>
2023-11-09 21:41:50 +0100

    Fix du get undefined avec l'id

--- library-site/src/app/books/[id]/page.tsx
+++ library-site/src/app/books/[id]/page.tsx
@@ -1,25 +1,29 @@
 'use client';
 
 import { FC, useEffect } from 'react';
+import { useParams } from 'next/navigation';
 import { useBookIdProviders } from '@/hooks/providers/bookIdProviders';
+
 
 const BookPage: FC = () => {
   const { useBookId } = useBookIdProviders();
   const { book, load } = useBookId();
+  const { id } = useParams();
 
   useEffect(() => {
-      load(); 
-  }, []);
+    load(id);
+  }, []); // Ajoutez une liste de dépendances vide ici pour que useEffect s'exécute une seule fois
 
   return (
     <>
       <h1>Page de détails du livre</h1>
-      {book.title && (
+      {book.name && (
         <div>
           <h2>{book.name}</h2>
-          <p>Auteur : {book.authorId}</p>
+          <p>Auteur : {book.author.firstName} {book.author.lastName}</p>
           <p>Date : {book.writtenOn}</p>
-          {/* Ajoutez d'autres informations sur le livre ici */}
+          <p>Genres : {book.genres.map((genre) => genre.name).join(', ')}</p>
+          {/* Vous pouvez ajouter d'autres informations sur le livre ici */}
         </div>
       )}
     </>
