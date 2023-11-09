115051e06396bda584e7f4264796e742f3c1aa41
Quentin AMBROZIEWICZ <quentin.ambroziewicz@student.junia.com>
2023-11-09 21:08:56 +0100

    Ajout de la photo Url en back-end

--- library-api/src/controllers/authors/author.dto.ts
+++ library-api/src/controllers/authors/author.dto.ts
@@ -2,4 +2,6 @@ export class CreateAuthorDto {
   firstName: string;
 
   lastName: string;
+
+  photoUrl?: string;
 }
--- library-site/src/models/author.model.ts
+++ library-site/src/models/author.model.ts
@@ -2,5 +2,5 @@ export type AuthorModel = {
     id: string;
     firstName: string;
     lastName: string;
-    photoUrl?: string;
+    photoUrl: string;
 };
