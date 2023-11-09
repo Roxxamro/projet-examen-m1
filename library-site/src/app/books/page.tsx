82ad7b7c64aa208d8a475b4d78e9e6cd5c5bf6a7
Quentin AMBROZIEWICZ <quentin.ambroziewicz@student.junia.com>
2023-11-09 20:30:07 +0100

    Fix de l'api sur le port 3000 !!!

--- library-site/src/app/books/page.tsx
+++ library-site/src/app/books/page.tsx
@@ -1,88 +1,94 @@
-import React, { FC, ReactElement, useEffect, useState } from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import { useBooksProviders } from '@/hooks';
 
-const BooksPage: FC = (): ReactElement => {
+const BooksPage: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const { useListBooks } = useBooksProviders();
   const { books, load } = useListBooks();
-  const [searchTerm, setSearchTerm] = useState<string>('');
-  const [selectedGenre, setSelectedGenre] = useState<string>('');
-  const [sortBy, setSortBy] = useState<string>('name');
 
   useEffect(() => {
-    load({ search: searchTerm, filterGenres: selectedGenre ? [selectedGenre] : [], sort: { field: sortBy, direction: 'asc' } });
-  }, [searchTerm, selectedGenre, sortBy]);
+    load(); // Charger les livres au montage initial du composant
+    console.log(books);
+  }, []); // Utiliser un tableau vide pour que l'effet se déclenche une seule fois
 
-  const genres = ['fantastique', 'comédie']; // Ajoutez ici d'autres genres au besoin
+  const handleBookClick = (id: string) => {
+    // Rediriger l'utilisateur vers la page de détails du livre
+    window.location.href = `/books/${id}`;
+  };
+
+  const handleAddBookClick = () => {
+    // Ouvrir la boîte de dialogue modale
+    setIsModalOpen(true);
+  };
 
   return (
-    <div className="container mx-auto p-4 flex h-screen">
-      <div className="w-1/4 h-full border-r" style={{ borderWidth: '2px', borderColor: '#ccc' }}>
-        <div className="mb-4">
-          <span className="mb-2 block">Filtrer par genre :</span>
-          {genres.map((genre) => (
-            <button
-              key={genre}
-              className={`mb-2 px-2 py-1 rounded border ${selectedGenre === genre ? 'bg-blue-500 text-white' : 'border-gray-300'}`}
-              onClick={() => setSelectedGenre(selectedGenre === genre ? '' : genre)}
-            >
-              {genre}
-            </button>
-          ))}
-          <div className="mt-4">
-            <span className="block font-semibold mb-2">Trier par :</span>
-            <button
-              className={`mr-2 px-2 py-1 rounded border ${sortBy === 'name' ? 'bg-blue-500 text-white' : 'border-gray-300'}`}
-              onClick={() => setSortBy('name')}
-            >
-              Titre
-            </button>
-            <button
-              className={`mr-2 px-2 py-1 rounded border ${sortBy === 'name author' ? 'bg-blue-500 text-white' : 'border-gray-300'}`}
-              onClick={() => setSortBy('name author')}
-            >
-              Auteur
-            </button>
-            <button
-              className={`mr-2 px-2 py-1 rounded border ${sortBy === 'writtenOn' ? 'bg-blue-500 text-white' : 'border-gray-300'}`}
-              onClick={() => setSortBy('writtenOn')}
-            >
-              Date
-            </button>
-            <button
-              className={`mr-2 px-2 py-1 rounded border ${sortBy === 'genres' ? 'bg-blue-500 text-white' : 'border-gray-300'}`}
-              onClick={() => setSortBy('genres')}
-            >
-              Genre
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="w-3/4 pl-4 h-full">
+    <div className="flex flex-col gap-y-10">
+      <div className="mx-6 mt-4 flex items-center bg-black-project rounded-full p-2 w-60 h-8">
+        <span className="inline-block mr-2">
+          <svg
+            className="h-6 w-6 text-white"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg">
+            <circle cx="10.5" cy="10.5" r="6" />
+            <line x1="15.5" y1="15.5" x2="20" y2="20" />
+          </svg>
+        </span>
+
         <input
-          className="w-full mb-4 px-4 py-2 rounded border border-gray-300 text-black"
+          className="flex-grow text-white bg-transparent outline-none px-4 rounded-full"
           type="text"
-          placeholder="Rechercher par titre..."
+          placeholder="Rechercher..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <h1 className="text-3xl font-bold mb-4 text-center">Livres</h1>
-        <div className="flex justify-center flex-wrap overflow-y-auto">
-          {books.map((book) => (
-            <div key={book.id} className="mb-8 p-4 border rounded shadow-lg mr-4">
-              <h2 className="text-2xl font-bold mb-2">{book.name}</h2>
-              <p className="mb-2">
-                <span className="font-semibold">Genre :</span> {book.genres.join(', ')}
-              </p>
-              <p className="mb-2">
-                <span className="font-semibold">Date :</span> {new Date(book.writtenOn).toLocaleDateString()}
-              </p>
-              <p>
-                <span className="font-semibold">Auteur :</span> {book.author.firstName} {book.author.lastName}
-              </p>
-            </div>
-          ))}
-        </div>
+        <svg
+          className="h-6 w-6 text-white"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+        </svg>
       </div>
+
+      <div className="flex flex-wrap gap-x-10 gap-y-10 pl-6 pr-6">
+        {books.map((book) => (
+          <div className="w-80 h-20 flex items-center border-2 border-gray-project rounded-lg" key={book.id}>
+            <div className="pl-3 h-16 w-20">
+              <img className="object-cover h-full w-full rounded-lg" src={book.author.photoUrl} alt={book.name} />
+            </div>
+            <div className="flex flex-col justify-center pl-3">
+              <p className="font-outfit font-semibold text-gray-project">{book.name}</p>
+              <p className="font-outfit text-gray-project">{`${book.author.firstName} ${book.author.lastName}`}</p>
+              <div className="flex">
+                {book.genres.map((genre, index) => (
+                  <span key={index} className="text-xs font-light text-gray-project mr-2">
+                    {genre}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <button aria-label="View book" className="ml-auto pr-5" onClick={() => handleBookClick(book.id)}>
+              <svg width="28px" height="28px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                {/* Icone pour afficher le livre */}
+                {/* ... (votre code d'icône) */}
+              </svg>
+            </button>
+          </div>
+        ))}
+      </div>
+
+      <span className="flex text-xs font-regular text-white items-center justify-center">
+        <button onClick={handleAddBookClick} aria-label="Add book" className="px-8 h-10 bg-green-project rounded-xl">
+          Ajouter un livre
+        </button>
+      </span>
     </div>
   );
 };
--- library-site/src/hooks/providers/bookProviders.ts
+++ library-site/src/hooks/providers/bookProviders.ts
@@ -1,67 +1,22 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { PlainBookModel, Sort } from '@/models';
-
-type ListBooksInput = {
-  search?: string;
-  sort?: Sort;
-  filterGenres?: string[];
-};
+import { useState } from 'react';
+import { PlainBookModel } from '@/models';
 
 type UseListBooksProvider = {
   books: PlainBookModel[];
-  load: (input?: ListBooksInput) => void;
+  load: () => void;
 };
 
 export const useListBooks = (): UseListBooksProvider => {
   const [books, setBooks] = useState<PlainBookModel[]>([]);
 
-  const fetchBooks = (input?: ListBooksInput): void => {
+  const fetchBooks = (): void => {
     axios
       .get(`${process.env.NEXT_PUBLIC_API_URL}/books`)
-      .then((response) => {
-        const books = response.data;
-
-        const sort = input?.sort ?? { field: 'id', direction: 'asc' };
-
-        const sortedBooks = (books as PlainBookModel[]).sort((b1, b2) => {
-            if (sort.field === 'name') {
-              if (sort.direction === 'asc') {
-                return b1.name.localeCompare(b2.name);
-              }
-              return b2.name.localeCompare(b1.name);
-            }
-            if (sort.field === 'name author') {
-              if (sort.direction === 'asc') {
-                return b1.author.firstName.localeCompare(b2.author.firstName);
-              }
-              return b2.author.firstName.localeCompare(b1.author.firstName);
-            }
-            if (sort.field === 'writtenOn') {
-              if (sort.direction === 'asc') {
-                return b1.writtenOn > b2.writtenOn ? 1 : -1;
-              }
-              return b2.writtenOn > b1.writtenOn ? 1 : -1;
-            }
-            if (sort.field === 'genres') {
-              if (sort.direction === 'asc') {
-                return b1.genres.join(',').localeCompare(b2.genres.join(','));
-              }
-              return b2.genres.join(',').localeCompare(b1.genres.join(','));
-            }
-            return 0;
-          })
-          .filter(
-           ({ name }) => (input?.search ? name.toLowerCase().includes(input.search.toLowerCase()) : true),
-          )
-          .filter(
-            ({ genres: bookGenres }) => (input?.filterGenres?.length ? bookGenres.some((genre) => input.filterGenres.includes(genre)) : true),
-          );
-
-        setBooks(sortedBooks);
-      })
+      .then((data) => setBooks(data.data))
       .catch((err) => console.error(err));
   };
+
   return { books, load: fetchBooks };
 };
 
