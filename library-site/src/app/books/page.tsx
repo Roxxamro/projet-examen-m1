a106525ae20f04908e600ef6b339e9b6ba7f12a0
Quentin AMBROZIEWICZ <quentin.ambroziewicz@student.junia.com>
2023-11-10 11:38:40 +0100

    Fix de useBooksProviders

--- library-site/src/app/books/page.tsx
+++ library-site/src/app/books/page.tsx
@@ -2,7 +2,7 @@
 
 
 import React, { useEffect, useState } from 'react';
-import { useBookIdProviders } from '@/hooks';
+import { useBooksProviders } from '@/hooks';
 
 
 const BooksPage: React.FC = () => {
