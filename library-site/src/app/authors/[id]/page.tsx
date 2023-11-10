648a7fe59e966431b6c844ab66f16c2afce51977
Simon DEMEILLIERS <simon.demeilliers@student.junia.com>
2023-11-10 16:26:34 +0100

    commit auteur details

--- library-site/src/app/authors/[id]/page.tsx
+++ library-site/src/app/authors/[id]/page.tsx
@@ -1,38 +1,114 @@
 'use client';
-
+import { FC, useEffect,useState } from 'react';
 import { useParams } from 'next/navigation';
-import { FC } from 'react';
+import { useAuthorIdProviders } from '@/hooks/providers/authorIdProviders';
 
-const AuthorDetailsPage: FC = () => {
+const AuthorDetailsPage: FC = () => { 
+  const { useAuthorId } = useAuthorIdProviders();
+  const { author, load, update} = useAuthorId();
   const { id } = useParams();
+  const [isEditing, setIsEditing] = useState(false);
+  const [newFirstName, setNewFirstName] = useState('');
+  const [newLastName, setNewLastName] = useState('');
+  const [newPhotoUrl, setNewPhotoUrl] = useState('');
+
+  useEffect(() => {
+    load(id);
+  }, []);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+    // Pré-remplir les champs avec les valeurs actuelles
+    setNewFirstName(author?.firstName || '');
+    setNewLastName(author?.lastName || '');
+    setNewPhotoUrl(author?.photoUrl || '');
+  };
+  const handleSaveClick = () => {
+    // Coté Back-end, on ne peut pas modifier les infos de l'auteur
+    update( {id, firstName: newFirstName, lastName: newLastName, photoUrl: newPhotoUrl });
+    setIsEditing(false);
+  };
+
 
   return (
-    <div className="flex flex-col justify-between h-screen">
-      <h1 className="text-2xl font-bold my-4">Author details &apos;ID&apos; NOT IMPLEMENTED</h1>
-      <div className="flex justify-center">
-      <button
-          onClick={() => { /* Fonction à exécuter lors du clic sur l'image en tant que bouton */ }}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-20 rounded mb-40"
-        >
-          MODIFIER
-        </button>
-      </div>
+    <div className="flex flex-col items-center justify-center h-screen">
+      {author ? (
+        <div className="text-center">
+          <h2 className="text-2xl font-bold mb-2">{`${author.firstName} ${author.lastName}`}</h2>
+          {isEditing ? (
+            <>
+              <input
+                type="text"
+                value={newFirstName}
+                onChange={(e) => setNewFirstName(e.target.value)}
+                placeholder="Nouveau prénom"
+                className="mb-2 px-2 py-1 text-black border rounded-md"
+              />
+              <input
+                type="text"
+                value={newLastName}
+                onChange={(e) => setNewLastName(e.target.value)}
+                placeholder="Nouveau nom"
+                className="mb-2 px-2 py-1 text-black border rounded-md"
+              />
+              <input
+                type="text"
+                value={newPhotoUrl}
+                onChange={(e) => setNewPhotoUrl(e.target.value)}
+                placeholder="Nouvelle URL de photo"
+                className="mb-2 px-2 py-1 text-black border rounded-md "
+              />
+              <button
+                onClick={handleSaveClick}
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mb-2"
+              >
+                ENREGISTRER
+              </button>
+            </>
+          ) : (
+            <>
+              {author.photoUrl && (
+                <img
+                  src={author.photoUrl}
+                  alt={`${author.firstName} ${author.lastName}`}
+                  className="rounded-full mx-auto mb-4"
+                />
+              )}
+              <button
+                onClick={handleEditClick}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-20 rounded mb-4"
+              >
+                MODIFIER
+              </button>
+            </>
+          )}
+        </div>
+      ) : (
+        <p>Chargement...</p>
+      )}
     </div>
   );
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 export default AuthorDetailsPage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- library-site/src/hooks/providers/authorIdProviders.ts
+++ library-site/src/hooks/providers/authorIdProviders.ts
@@ -1,29 +1,34 @@
 import axios from 'axios';
 import { useState } from 'react';
 import { AuthorModel } from '@/models';
-
+ 
 type UseAuthorIdProvider = {
-    author: AuthorModel;
-    load: (id: string) => void;
+  author: AuthorModel;
+  load: (id: string) => void;
+  update: (updatedAuthor: AuthorModel) => void;
 };
-
+ 
 export const useAuthorId = (): UseAuthorIdProvider => {
-    const [author, setAuthor] = useState<AuthorModel>({} as AuthorModel);
-
-    const fetchAuthor = (id: string): void => {
-        axios
-        .get(`${process.env.NEXT_PUBLIC_API_URL}/authors/${id}`)
-        .then((data) => setAuthor(data.data))
-        .catch((err) => console.error(err));
-    };
-
-    return { author, load: fetchAuthor };
-}
-
-type AuthorIdProviders = {
-    useAuthorId: () => UseAuthorIdProvider;
+  const [author, setAuthor] = useState<AuthorModel>({} as AuthorModel);
+ 
+  const fetchAuthor = (id: string): void => {
+    axios
+      .get(`${process.env.NEXT_PUBLIC_API_URL}/authors/${id}`)
+      .then((data) => setAuthor(data.data))
+      .catch((err) => console.error(err));
+  };
+ 
+  const updateAuthor = (updatedAuthor: AuthorModel): void => {
+    setAuthor(updatedAuthor);
+  };
+ 
+  return { author, load: fetchAuthor, update: updateAuthor };
 };
-
-export const useAuthorByIdProviders = (): AuthorIdProviders => ({
-    useAuthorId,
+ 
+type AuthorIdProviders = {
+  useAuthorId: () => UseAuthorIdProvider;
+};
+ 
+export const useAuthorIdProviders = (): AuthorIdProviders => ({
+  useAuthorId,
 });
